ffd5dc745abe104067d897e003c766f12c17f797
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2022-11-11 16:52:58 +0200

    Fixed bugs

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,19 +4,19 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('carte'),
+    menuList: document.querySelector('.carte'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', tremoveMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.bodymenu.classList.toggle('no-scroll');
+    refs.body.classList.toggle('no-scroll');
   }
   function removeMenu() {
     refs.menu.classList.add('is-hidden');
-    refs.bodymenu.classList.remove('no-scroll');
+    refs.body.classList.remove('no-scroll');
   }
 })();
